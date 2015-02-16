da2ad1c52cd057af1ce665d21905f61c8585164e
Jianjun Zhu <jianjun.zhu@intel.com>
2015-02-16 08:44:27 +0800

    Allow clients to connect signaling server with or without namespace.

--- peerserver.js
+++ peerserver.js
@@ -61,82 +61,83 @@ function emitChatEvent(targetUid, eventName, message, successCallback, failureCa
       failureCallback(2201);
 }
 
-function listen(io) {
-  io.of('/webrtc').use(function(socket,next){
-    var query=url.parse(socket.request.url,true).query;
-    var token=query.token;
-    var clientVersion=query.clientVersion;
-    var clientType=query.clientType;
-    switch(clientVersion){
-      case '2.0':
-      case '2.0.1':
-        /* Handshake stores session related information. Handshake data has following properties:
-         * user - property: id
-         * peers - a map, key is peer's uid, value is an object with property: conversation id.
-         * chat - property: id
-        */
-        if(token){
-          validateUser(token, function(uid){  // Validate user's token successfully.
-            socket.user={id:uid};
-            console.log(uid+' authentication passed.');
-          },function(error){
-              // Invalid login.
-              console.log('Authentication failed.');
-              next();
-          });
-        }else{
-          socket.user=new Object();
-          socket.user.id=createUuid()+'@anonymous';
-          console.log('Anonymous user: '+socket.user.id);
-        }
-        next();
-        break;
-      default:
-        next(new Error('2103'));
-        console.log('Unsupported client. Client version: '+query.clientVersion);
-        break;
-    }
-  });
-
-  io.of('/webrtc').on('connection',function(socket){
-    // Disconnect previous session if this user already signed in.
-    var uid=socket.user.id;
-    disconnectClient(uid);
-    sessionMap[uid]=socket;
-    socket.emit('server-authenticated',{uid:uid});  // Send current user's id to client.
-    console.log('A new client has connected. Online user number: '+Object.keys(sessionMap).length);
-
-    socket.on('disconnect',function(){
-      if(socket.user){
-        var uid=socket.user.id;
-        // Delete session
-        if(socket===sessionMap[socket.user.id]){
-          delete sessionMap[socket.user.id];
-        }
-        console.log(uid+' has disconnected. Online user number: '+Object.keys(sessionMap).length);
+function authorization(socket, next){
+  var query=url.parse(socket.request.url,true).query;
+  var token=query.token;
+  var clientVersion=query.clientVersion;
+  var clientType=query.clientType;
+  switch(clientVersion){
+    case '2.0':
+    case '2.0.1':
+      // socket.user stores session related information.
+      if(token){
+        validateUser(token, function(uid){  // Validate user's token successfully.
+          socket.user={id:uid};
+          console.log(uid+' authentication passed.');
+        },function(error){
+            // Invalid login.
+            console.log('Authentication failed.');
+            next();
+        });
+      }else{
+        socket.user=new Object();
+        socket.user.id=createUuid()+'@anonymous';
+        console.log('Anonymous user: '+socket.user.id);
       }
-    });
+      next();
+      break;
+    default:
+      next(new Error('2103'));
+      console.log('Unsupported client. Client version: '+query.clientVersion);
+      break;
+  }
+}
 
-    // Forward events
-    var forwardEvents=['chat-invitation','chat-accepted','stream-type','chat-negotiation-needed','chat-negotiation-accepted','chat-stopped','chat-denied','chat-signal'];
-    for (var i=0;i<forwardEvents.length;i++){
-      socket.on(forwardEvents[i],(function(i){
-        return function(data, ackCallback){
-          console.log('Received '+forwardEvents[i]);
-          data.from=socket.user.id;
-          var to=data.to;
-          delete data.to;
-          emitChatEvent(to,forwardEvents[i],data,function(){
-            if(ackCallback)
-              ackCallback();
-          },function(errorCode){
-            if(ackCallback)
-              ackCallback(errorCode);
-          });
-        };
-      })(i));
+function onConnection(socket){
+  // Disconnect previous session if this user already signed in.
+  var uid=socket.user.id;
+  disconnectClient(uid);
+  sessionMap[uid]=socket;
+  socket.emit('server-authenticated',{uid:uid});  // Send current user's id to client.
+  console.log('A new client has connected. Online user number: '+Object.keys(sessionMap).length);
+
+  socket.on('disconnect',function(){
+    if(socket.user){
+      var uid=socket.user.id;
+      // Delete session
+      if(socket===sessionMap[socket.user.id]){
+        delete sessionMap[socket.user.id];
+      }
+      console.log(uid+' has disconnected. Online user number: '+Object.keys(sessionMap).length);
     }
   });
+
+  // Forward events
+  var forwardEvents=['chat-invitation','chat-accepted','stream-type','chat-negotiation-needed','chat-negotiation-accepted','chat-stopped','chat-denied','chat-signal'];
+  for (var i=0;i<forwardEvents.length;i++){
+    socket.on(forwardEvents[i],(function(i){
+      return function(data, ackCallback){
+        console.log('Received '+forwardEvents[i]);
+        data.from=socket.user.id;
+        var to=data.to;
+        delete data.to;
+        emitChatEvent(to,forwardEvents[i],data,function(){
+          if(ackCallback)
+            ackCallback();
+        },function(errorCode){
+          if(ackCallback)
+            ackCallback(errorCode);
+        });
+      };
+    })(i));
+  }
+}
+
+function listen(io) {
+  io.use(authorization);
+  io.on('connection',onConnection);
+  io.of('/webrtc').use(authorization);
+  io.of('/webrtc').on('connection',onConnection);
 }
 
 listen(io);
