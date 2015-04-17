1fa7c8bedd256d0c47bf685b8c687746534a9933
Jianjun Zhu <jianjun.zhu@intel.com>
2015-04-17 09:59:11 +0800

    Update default server address and remove old files.
    Change-Id: I39b11ca01871880466c6d7b5ecd8a431eed3047d

--- peerserver.js
+++ peerserver.js
@@ -68,6 +68,7 @@ function authorization(socket, next){
   var clientType=query.clientType;
   switch(clientVersion){
     case '2.1':
+    case '2.1.1':
       // socket.user stores session related information.
       if(token){
         validateUser(token, function(uid){  // Validate user's token successfully.
