6379826ad4f081e631427e46eb77b359f287470c
Jianjun Zhu <jianjun.zhu@intel.com>
2016-09-21 11:10:09 +0800

    Add version number 3.2.1.
    Change-Id: I5c03bc272da7617780ac506d7231f3e49b9513f1
    Reviewed-on: https://git-ccr-1.devtools.intel.com/gerrit/40948
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Xian, Yuqiang <yuqiang.xian@intel.com>

--- peerserver.js
+++ peerserver.js
@@ -94,6 +94,7 @@ function authorization(socket, next){
   var clientType=query.clientType;
   switch(clientVersion){
     case '3.2':
+    case '3.2.1':
       // socket.user stores session related information.
       if(token){
         validateUser(token, function(uid){  // Validate user's token successfully.
