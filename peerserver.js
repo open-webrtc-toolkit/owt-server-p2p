40c135109113641e69c066d606f10a23ebb320fb
Jianjun Zhu <jianjun.zhu@intel.com>
2017-02-24 16:47:58 +0800

    Add version 3.3.1.
    Change-Id: I04c0b6932e87622c16a9496121adb39c750f6464
    Reviewed-on: https://git-ccr-1.devtools.intel.com/gerrit/51782
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Qiu, Jianlin <jianlin.qiu@intel.com>

--- peerserver.js
+++ peerserver.js
@@ -96,6 +96,7 @@ function authorization(socket, next){
     case '3.2':
     case '3.2.1':
     case '3.3':
+    case '3.3.1':
       // socket.user stores session related information.
       if(token){
         validateUser(token, function(uid){  // Validate user's token successfully.
