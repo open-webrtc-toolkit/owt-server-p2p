931c2656984ef11cf2e76f5ad81954dce0c1cd2a
Jianjun Zhu <jianjun.zhu@intel.com>
2018-09-20 10:30:03 +0800

    Add version 4.1.
    Change-Id: I25ff9bfd5b7fc71d15a73d39f2c9f6d7df210a89
    Reviewed-on: https://git-ccr-1.devtools.intel.com/gerrit/99651
    Reviewed-by: Zhang, He <he.zhang@intel.com>
    Tested-by: Zhang, He <he.zhang@intel.com>
    Tested-by: Qiu, Jianlin <jianlin.qiu@intel.com>

--- peerserver.js
+++ peerserver.js
@@ -96,6 +96,7 @@ function authorization(socket, next){
     case '4.0':
     case '4.0.1':
     case '4.0.2':
+    case '4.1':
       // socket.user stores session related information.
       if(token){
         validateUser(token, function(uid){  // Validate user's token successfully.
