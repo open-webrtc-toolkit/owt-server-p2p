c3a68dab93d5fe9d2c7b0162aae3db581e8b9a63
Jianjun Zhu <jianjun.zhu@intel.com>
2019-03-26 14:58:13 +0800

    Add version 4.2.

--- peerserver.js
+++ peerserver.js
@@ -98,6 +98,7 @@ function authorization(socket, next){
     case '4.0.2':
     case '4.1':
     case '4.1.1':
+    case '4.2':
       // socket.user stores session related information.
       if(token){
         validateUser(token, function(uid){  // Validate user's token successfully.
