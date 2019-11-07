be2919997b6c4917c5c01a31955d2d3c809e26c1
Jianjun Zhu <jianjun.zhu@intel.com>
2019-11-07 17:58:51 +0800

    Refactor P2P server.
    - Remove vendormodule as P2P server is open sourced. Developers may
      change authentication procedure by modifying source code.
    - Remove unused code.
    - Use modern JavaScript syntax.
    - Add format and lint rules.

--- peerserver.js
+++ peerserver.js
@@ -2,153 +2,132 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-// Prepare for web server
-var fs = require("fs");
-var path = require("path");
-var url = require('url');
-var config = require('./config');
-var account = require('./vendormodule');
+'use strict';
 
-var dirname = __dirname || path.dirname(fs.readlinkSync('/proc/self/exe'));
-var httpsOptions = {
+// Prepare for web server.
+const fs = require("fs");
+const path = require("path");
+const url = require('url');
+const config = require('./config');
+const forwardEventName = 'owt-message';
+
+const dirname = __dirname || path.dirname(fs.readlinkSync('/proc/self/exe'));
+const httpsOptions = {
   key: fs.readFileSync(path.resolve(dirname, 'cert/key.pem')).toString(),
   cert: fs.readFileSync(path.resolve(dirname, 'cert/cert.pem')).toString()
 };
+const app = require('express')();
+const plainServer = require('socket.io').listen(app.listen(config.port.plain));
+const secureServer = require('socket.io').listen(require('https').createServer(
+  httpsOptions, app).listen(config.port.secured));
 
-var app = require('express')();
-var server = app.listen(config.port.plain);
-var servers = require("https").createServer(httpsOptions, app).listen(config.port.secured);
-var io = require('socket.io').listen(server);
-var ios = require('socket.io').listen(servers);
+const sessionMap = new Map();  // Key is uid, and value is session object.
 
-var sessionMap = {};  // Key is uid, and value is session object.
-
-// Check user's token from partner
-function validateUser(token, successCallback, failureCallback){
-  // TODO: Should check token first, replace this block when engagement with different partners.
-  if(token){
-    account.authentication(token,function(uid){
-      successCallback(uid);
-    },function(){
-      console.log('Account system return false.');
-      failureCallback(0);
-    });
-  }
-  else
-    failureCallback(0);
-}
-
-function disconnectClient(uid){
-  if(sessionMap[uid]!==undefined){
-    var session=sessionMap[uid];
+function disconnectClient(uid) {
+  if (sessionMap.has(uid)) {
+    const session = sessionMap.get(uid);
     session.emit('server-disconnect');
     session.disconnect();
-    console.log('Force disconnected '+uid);
+    console.log('Force disconnected ' + uid);
   }
 }
 
 function createUuid(){
   return 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+    const r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
     return v.toString(16);
   });
 }
 
-function emitChatEvent(targetUid, eventName, message, successCallback, failureCallback){
-  if(sessionMap[targetUid]){
-    sessionMap[targetUid].emit(eventName,message);
-    if(successCallback)
-      successCallback();
+function emitChatEvent(targetUid, eventName, message) {
+  if (sessionMap.has(targetUid)) {
+    sessionMap.get(targetUid).emit(eventName, message);
+    return Promise.resolve();
+  } else {
+    // Remote endpoint cannot be reached.
+    return Promise.reject(2201);
   }
-  else
-    if(failureCallback)
-      failureCallback(2201);
 }
 
-function authorization(socket, next){
-  var query=url.parse(socket.request.url,true).query;
-  var token=query.token;
-  var clientVersion=query.clientVersion;
-  var clientType=query.clientType;
-  switch(clientVersion){
+// Please modify this function if you need to add your own credential validation
+// procedure.
+function authenticate(socket, next) {
+  const query = url.parse(socket.request.url, true).query;
+  const token = query.token;
+  const clientVersion = query.clientVersion;
+  const clientType = query.clientType;
+  switch (clientVersion) {
     case '4.2':
     case '4.2.1':
     case '4.3':
       // socket.user stores session related information.
-      if(token){
-        validateUser(token, function(uid){  // Validate user's token successfully.
-          socket.user={id:uid};
-          console.log(uid+' authentication passed.');
-        },function(error){
-            // Invalid login.
-            console.log('Authentication failed.');
-            next();
-        });
-      }else{
-        socket.user=new Object();
-        socket.user.id=createUuid()+'@anonymous';
-        console.log('Anonymous user: '+socket.user.id);
+      if (token) {
+        // Add credential validation here.
+        socket.user = {
+          id: token
+        };
+        console.log(token + ' is connected.');
+      } else {
+        // Empty token is allowed, and an random token is issued here.
+        socket.user = new Object();
+        socket.user.id = createUuid() + '@anonymous';
+        console.log('Anonymous user: ' + socket.user.id);
       }
       next();
       break;
     default:
       next(new Error('2103'));
-      console.log('Unsupported client. Client version: '+query.clientVersion);
+      console.log('Unsupported client. Client version: ' + query.clientVersion);
       break;
   }
 }
 
 function onConnection(socket){
   // Disconnect previous session if this user already signed in.
-  var uid=socket.user.id;
+  const uid = socket.user.id;
   disconnectClient(uid);
-  sessionMap[uid]=socket;
+  sessionMap.set(uid, socket);
   socket.emit('server-authenticated',{uid:uid});  // Send current user's id to client.
-  console.log('A new client has connected. Online user number: '+Object.keys(sessionMap).length);
+  console.log(
+      'A new client has connected. Online user number: ' + sessionMap.size);
 
   socket.on('disconnect',function(){
     if(socket.user){
-      var uid=socket.user.id;
-      // Delete session
-      if(socket===sessionMap[socket.user.id]){
-        delete sessionMap[socket.user.id];
+      const uid = socket.user.id;
+      // Delete session.
+      if (sessionMap.has(socket.user.id)) {
+        delete sessionMap.delete(socket.user.id);
       }
-      console.log(uid+' has disconnected. Online user number: '+Object.keys(sessionMap).length);
+      console.log(
+          uid + ' has disconnected. Online user number: ' + sessionMap.size);
     }
   });
 
-  // Forward events
-  var forwardEvents=['owt-message'];
-  for (var i=0;i<forwardEvents.length;i++){
-    socket.on(forwardEvents[i],(function(i){
-      return function(data, ackCallback){
-        data.from=socket.user.id;
-        var to=data.to;
-        delete data.to;
-        emitChatEvent(to,forwardEvents[i],data,function(){
-          if(ackCallback)
-            ackCallback();
-        },function(errorCode){
-          if(ackCallback)
-            ackCallback(errorCode);
-        });
-      };
-    })(i));
-  }
+  socket.on(forwardEventName, (data, ackCallback) => {
+    data.from = socket.user.id;
+    const to = data.to;
+    delete data.to;
+    emitChatEvent(to, forwardEventName, data).then(() => {
+      ackCallback();
+    }, (errorCode) => {
+      ackCallback(errorCode);
+    });
+  });
 }
 
 function listen(io) {
-  io.use(authorization);
+  io.use(authenticate);
   io.on('connection',onConnection);
 }
 
-listen(io);
-listen(ios);
+listen(plainServer);
+listen(secureServer);
 
 // Signaling server only allowed to be connected with Socket.io.
 // If a client try to connect it with any other methods, server returns 405.
-/*app.get('*', function(req, res, next) {
-  res.send(405, 'WebRTC signaling server. Please connect it with Socket.IO.');
-});*/
+app.get('*', function(req, res, next) {
+  res.send(405, 'OWT signaling server. Please connect it with Socket.IO.');
+});
 
-console.info('Listening port: ' + config.port.plain + '/' + config.port.secured);
+console.info('Listening ports: ' + config.port.plain + '(plain) and ' + config
+  .port.secured + '(secure).');
